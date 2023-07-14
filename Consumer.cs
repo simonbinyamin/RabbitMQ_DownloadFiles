4a2873c05eb545685533a4a911c5033f044d8c3f
Simon <simonbinyamin@gmail.com>
2023-07-14 06:31:52 +0200

    Add project files.

---
+++ Consumer.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ_DownloadFiles
+{
+
+
+    class Consumer
+    {
+        static void Main()
+        {
+            var connfactory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var conn = connfactory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare(queue: "my_stocks",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, b) =>
+                {
+                    var body = b.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("received: {0}", message);
+                };
+
+                channel.BasicConsume(queue: "my_stocks",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine("exit.");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
---
+++ Publisher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQ_DownloadFiles
+{
+    public class Publisher
+    {
+        static void Main0()
+        {
+            var connfactory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var conn = connfactory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare(queue: "my_stocks",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Yooo!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "my_stocks",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine("sent: {0}", message);
+            }
+
+            Console.WriteLine("exit.");
+            Console.ReadLine();
+        }
+    }
+}
